857c19a944f8e3e304c1827d60b4aaaa02f23f84
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2019-07-15 23:25:51 +0700

    Create w4hw.cpp

---
+++ w4hw.cpp
@@ -0,0 +1,30 @@
+﻿//โปรแกรมคำนวณเกรด
+#include<iostream>
+using namespace std;
+int main()
+{
+	int S;
+	cout << "Enter your score (0-100) : ";
+	cin >> S;
+	cout << "Your get grade : ";
+	if (( S >= 0 ) && ( S <= 100)){
+			if (( S >= 90) && ( S <= 100 )){
+				cout << "A" << endl;
+				}
+			else if (( S >= 80) && ( S <= 89)){
+				cout << "B" << endl;
+				}
+			else if (( S >= 70) && ( S <= 79)){
+				cout << "C" << endl;
+				}
+			else if (( S >= 60) && ( S <= 69)){
+				cout << "D" << endl;
+				}
+			else ( S <= 59),cout << "F" << endl;
+		}
+	else {cout << "Error!" << endl;
+		 cout << "You over score" << endl;
+		}
+	return(0);
+	
+}
